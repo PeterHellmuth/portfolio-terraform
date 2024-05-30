eb5d5960aa6b81c638f8ce30f983f1a3a5c0d401
Peter <10523853+PeterHellmuth@users.noreply.github.com>
2024-05-30 08:46:10 -0500

    Basic EC2 infrastructure with 22 ingress for ssh and 8080 ingress for reroute to https server

---
+++ main.tf
@@ -0,0 +1,69 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+  required_version = ">= 1.2.0"
+}
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_security_group" "main" {
+  egress = [
+    {
+      cidr_blocks      = [ "0.0.0.0/0", ]
+      description      = ""
+      from_port        = 0
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "-1"
+      security_groups  = []
+      self             = false
+      to_port          = 0
+    }
+  ]
+ ingress                = [
+   {
+     cidr_blocks      = [ "0.0.0.0/0", ]
+     description      = "ssh"
+     from_port        = 22
+     ipv6_cidr_blocks = []
+     prefix_list_ids  = []
+     protocol         = "tcp"
+     security_groups  = []
+     self             = false
+     to_port          = 22
+  },
+     {
+     cidr_blocks      = [ "0.0.0.0/0", ]
+     description      = "https-server"
+     from_port        = 8080
+     ipv6_cidr_blocks = []
+     prefix_list_ids  = []
+     protocol         = "tcp"
+     security_groups  = []
+     self             = false
+     to_port          = 8080
+  }
+  ]
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0cf2b4e024cdb6960"
+  instance_type = "t2.micro"
+  count = 1
+  tags = {
+    Name = "portfolio"
+  }
+  vpc_security_group_ids = [aws_security_group.main.id]
+  associate_public_ip_address = true
+  key_name="ssh-key"
+}
+
+output "instance_ip" {
+  description = "The public ip for ssh access"
+  value       = aws_instance.app_server[0].public_ip
+}
